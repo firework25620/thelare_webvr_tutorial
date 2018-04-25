950368aec90dddf4c95946f7c94b2fa9ef083ce7
firework25620 <38630600+firework25620@users.noreply.github.com>
2018-04-25 17:41:59 -0400

    Update theLareCommonCode.js

--- public/theLareCommonCode.js
+++ public/theLareCommonCode.js
@@ -10,19 +10,7 @@ var drawingRoom = vRViewPlayer('#first-vr-example', {
 
 window.addEventListener('load', drawingRoom);
 
-var roomDrawing = vRViewPlayer('#second-vr-example', {
-  image: 'http://25dkd82t2owl3yew6h49ewsh.wpengine.netdna-cdn.com/wp-content/uploads/2014/08/JIC-photo-623x200.jpg',
-  is_stereo: false,
-});
 
-window.addEventListener('load', roomDrawing);
-
-var boxRoomTemplate = vRViewPlayer('#third-vr-example', {
-  image: 'https://fscl01.fonpit.de/userfiles/4155894/image/photo-sphere-w628.jpg',
-  is_stereo: false,
-});
-
-window.addEventListener('load', boxRoomTemplate);
 
 
 var setReadyEvents = function(event) {
